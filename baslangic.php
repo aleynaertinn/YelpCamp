dba7ab6e042bb9f7a86d340d0f49f723735a284a
Aleyna Ertin <76805724+aleynaertinn@users.noreply.github.com>
2022-12-18 18:14:10 +0300

    Add files via upload

---
+++ baslangic.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chirp | Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+	<title>YelpCamp</title>
+</head>
+    <body>
+        <div class="header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-7">
+                        <div class="text_b">
+                            <img src="Assets/Logo.svg">
+                            <h1>Explore the best <span>camps on Earth.</span></h1>
+                            <p>YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</p>
+                            <div class="text_ul">
+                                <ul>
+                                    <li><i class='bx bxs-check-circle'></i>Add your own camp suggestions</li>
+                                    <li><i class='bx bxs-check-circle'></i>Leaven reviews and experiences</li>
+                                    <li><i class='bx bxs-check-circle'></i>See locations for all camps.</li>
+                                </ul>
+                            </div>
+                            <button><a href="http://localhost/YelpCamp/" class="ileri_btn">View Campgrounds</a></button>
+                            <div class="partner">
+                                <p>Partnered with:</p>
+                                <div class="p_img">
+                                    <img src="Assets/Airbnb.svg">
+                                    <img src="Assets/Booking.svg">
+                                    <img src="Assets/Plum%20Guide.svg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-5">
+                        <div class="b_img">
+                            <img src="Assets/Hero%20Image.jpg">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    
+    
+    
+    </body>
